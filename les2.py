3f2aae718366dfa796d025a8fe776651b07c892f
victoriia.liubchenko <bumerok2005@gmail.com>
2022-11-13 16:49:32 +0200

    Add file

---
+++ les2.py
@@ -0,0 +1,12 @@
+month = int(input("Enter month: "))
+
+if month >=3 and month <6:
+    print("Spring")
+elif month >= 6 and month < 9:
+    print("summer")
+elif month >=9  and month <12:
+    print("autumn")
+elif month == 12 or month == 1 or month == 2:
+    print("Winter")
+else:
+    print("no such month")
