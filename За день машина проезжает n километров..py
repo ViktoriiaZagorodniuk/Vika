c71287b72ab64de7c2dd9917d38d2d001aa268bc
victoriia.liubchenko <bumerok2005@gmail.com>
2022-11-13 22:32:14 +0200

    Add file252

---
+++ За день машина проезжает n километров..py
@@ -0,0 +1,27 @@
+#  За день машина проезжает n километров. Сколько дней нужно, чтобы
+# проехать маршрут длиной m километров? Программа получает на вход
+# числа n и m.
+
+
+
+n = 800
+m = 4000
+d=(-m)//n
+print(-d)
+
+
+# n = int(input(700))
+# m = int(input(2100))
+# d = m//n
+#     print(d)
+
+
+# dis1 = int(input(600))
+# dis2 = int(input(2100))
+#     print((dis2+dis1-1)//dis1)
+
+
+# import math
+# n=int(input(700))
+# m=int(input(2100))
+# print(math.ceil(m/n))
