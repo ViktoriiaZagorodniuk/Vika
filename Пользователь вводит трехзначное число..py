c0a7e70bc6152d063bb2cad9d21f6ff05b30e734
victoriia.liubchenko <bumerok2005@gmail.com>
2022-11-14 19:48:27 +0200

    Add file252

--- Пользователь вводит трехзначное число..py
+++ Пользователь вводит трехзначное число..py
@@ -1,19 +1,20 @@
 # Пользователь вводит трехзначное число. Найдите сумму его цифр.
 # (используйте %)
 
-# number = int(input("Please enter number : "))
-# a = int(n[0])
-# b = int(n[1])
-# c = int(n[2])
-#     print("sum:", a + b + c)
-
-
 n = int(input("Please enter number : "))
-c = n % 10
-n = n // 10
-b = n % 10
-a = n // 10
-    print("sum:", a + b + c)
+a = int(n[0])
+b = int(n[1])
+c = int(n[2])
+    print(a + b + c)
+
+
+# n = int(input("Please enter number : "))
+# c = n % 10
+# n = n // 10
+# b = n % 10
+# a = n // 10
+#     result = sun(int(input n)) for i in range(n))
+#     print(result)
 
 
 
@@ -24,3 +25,23 @@ a = n // 10
 # n = n // 10
 # sum += d
 #     print('sum is ', sum)
+
+
+# str1=str(input())
+# print(str1)
+# k=0
+# for elem in str1:
+#     if elem.isdigit():
+#         k += int(elem)
+#         print(elem)
+# print("k=" ,k)
+
+
+# найти сумму четных чисел в строке
+# s = input()
+# sum = 0
+# for i in range(len(s)):
+#     if int(s[i]) % 2 == 0:
+#         sum += int(s[i])
+#     print(sum)
+
